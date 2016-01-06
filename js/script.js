ff6f396225dba6119cbfcffdf9269aac17627abb
kvp-natarajab <nataraja.b@kvpcorp.com>
2016-01-06 19:04:01 +0530

    First Task

---
+++ js/script.js
@@ -0,0 +1,127 @@
+var msg="";
+var isTrue=false;
+$(function(){
+
+
+	$("#signin-form").submit(function(event){
+		var firstName=$("#fname").val();
+		var lastName=$("#lname").val();
+		var email=$("#email").val();
+		var password=$("#password").val();
+		var fn=validate_firstname(firstName);
+		var ln=validate_lastname(lastName);
+		var em=validate_email(email);
+		var ps=validate_password(password);
+
+		if(fn&&ln&&em&&ps){
+
+			return true;
+		}else{
+			isTrue=false;
+			return false;
+		}
+
+	});
+
+	$("#closeMsg").click(function(){
+		$("#error").hide(500);
+	});
+
+	$("#signInPassword").keypress(function(){
+		 $("#signInSubmit").prop("disabled",false);
+	});
+	remove_error();
+
+});
+	
+
+function remove_error(){
+	$("#fname").keypress(function(){
+		$(this).css("border","1px solid #BFBFBF");
+	});
+	$("#lname").keypress(function(){
+		$(this).css("border","1px solid #BFBFBF");
+	});
+	$("#email").keypress(function(){
+		$(this).css("border","1px solid #BFBFBF");
+	});
+	$("#password").keypress(function(){
+		$(this).css("border","1px solid #BFBFBF");
+	});
+	
+}
+
+function validate_firstname(firstName){
+	
+	if(firstName==""){
+			if(!isTrue){
+				msg="Please enter your first name";
+				isTrue=true;
+			}
+			$("#fname").css("border","1px solid red");
+			call_printmsg(msg);
+			return false;
+	}else{
+			$("#fname").css("border","1px solid grey");
+			return true;
+	}
+}
+
+function validate_lastname(lastName){
+	if(lastName==""){
+		if(!isTrue){
+			msg="Please enter your last name";
+			isTrue=true;
+		}
+		$("#lname").css("border","1px solid red");
+		call_printmsg(msg);
+		return false;
+	}else{
+		$("#lname").css("border","1px solid grey");
+		return true;
+	}
+}
+
+function validate_email(email){
+	if(email==""){
+			if(!isTrue){
+				msg="Please enter your email";
+				isTrue=true;
+			}
+			$("#email").css("border","1px solid red");
+			call_printmsg(msg);
+			return false;
+	}else{
+		
+		return true;
+	}
+}
+
+function validate_password(password){
+		var err=true;
+		if(password==""){
+			if(!isTrue){
+				
+				msg="Please enter your Password";
+			}
+			err=false;
+		}else if(password.length<6){
+			if(!isTrue){
+				
+				msg="Password must conatin 6 or more characters";
+			}
+			err=false;
+		}
+		if(err){
+			return true;
+		}else{
+			$("#password").css("border","1px solid red");
+			call_printmsg(msg);
+			return false;
+		}
+		
+}
+function call_printmsg(msg){
+			$("#error").show(500);
+			$("#error-msg").text(msg);
+}
